0a83874465e86d4a1e1bd23ac26d1e8b223e528a
oleksandr.pleshakov <oleksandr.pleshakov@horiba.com>
2025-01-30 16:17:08 +0200

    test

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,14 @@ import { sync } from "glob";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "https://apleshakov88.github.io/vite-test01/",
   appType: "mpa",
   root: "./src",
   build: {
     outDir: "../dist",
     emptyOutDir: true,
     rollupOptions: {
-      input: sync("./src/**/*.html".replace(/\\/g, "/"))
+      input: sync("./src/**/*.html".replace(/\\/g, "/")),
     },
   },
 });
