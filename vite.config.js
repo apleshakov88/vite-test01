ef7d7615c29b763f911538886f9a411e92a43df5
oleksandr.pleshakov <oleksandr.pleshakov@horiba.com>
2025-01-30 16:20:20 +0200

    test

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { sync } from "glob";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "https://apleshakov88.github.io/vite-test01/",
+  base: "https://apleshakov88.github.io/vite-test01/dist",
   appType: "mpa",
   root: "./src",
   build: {
